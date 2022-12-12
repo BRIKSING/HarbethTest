9f74f1187b0de7ec818f431da64570d15d908d11
BRIKSING <BRIKSING@yandex.ru>
2022-12-12 21:21:41 +0300

    Test commit 2

---
+++ HarbethTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarbethTest
+//
+//  Created by Admin on 12.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
